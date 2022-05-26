c73cfe0b4207d27abdebb3236874fdb5d48b5ffa
ali hijjawi <ajhijjawi@gmail.com>
2022-05-26 23:11:22 +0300

    homepage

--- softly-architecturing/src/pages/about.js
+++ softly-architecturing/src/pages/about.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../static/css/homepage.css"
+import stickman from "../static/images/stickman.png"
 
 const About = () => {
-return (
-	<div>
-	<h1>
-		GeeksforGeeks is a Computer
-		Science portal for geeks.
-	</h1>
-	</div>
-);
+	return (
+		<>
+		<div class="div">
+			<p class="title">Homepage Title</p>
+			<div id="single" class="left">
+				<Link to="/account"><p class="text">ACCOUNT</p></Link>
+			</div>
+			<div id="tutorial" class="right">
+				<Link to="/bank"><p class="text">BANK</p></Link>
+			</div>
+			<div id="options" class="bottomL">
+				<Link to="/store"><p class="text">STORE</p></Link>
+			</div>
+			<div id="credits" class="bottomR">
+				<Link to="/something"><p class="text">something</p></Link>
+			</div>
+		</div>
+		<div class="user">
+			<img class = "img" src={stickman} alt="user"/>
+		</div>
+		</>
+	);
 };
 
 export default About;
